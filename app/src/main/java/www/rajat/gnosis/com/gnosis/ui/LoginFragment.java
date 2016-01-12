221ef12b5ec516356c35339ca8873dcb1ea8957a
Rajat Vijay <rajatvijay5@gmail.com>
2016-01-13 00:53:56 +0530

    Added Tab Layout and UI for Login and Sign Up

---
+++ app/src/main/java/www/rajat/gnosis/com/gnosis/ui/LoginFragment.java
@@ -0,0 +1,57 @@
+package www.rajat.gnosis.com.gnosis.ui;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import www.rajat.gnosis.com.gnosis.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LoginFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LoginFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LoginFragment.
+     */
+    public static LoginFragment newInstance() {
+        LoginFragment fragment = new LoginFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+
+}
--- app/src/main/java/www/rajat/gnosis/com/gnosis/ui/MainActivity.java
+++ app/src/main/java/www/rajat/gnosis/com/gnosis/ui/MainActivity.java
@@ -1,11 +1,13 @@
 package www.rajat.gnosis.com.gnosis.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -17,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -27,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+
+        initializeScreen();
     }
 
     @Override
@@ -50,5 +54,85 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Link layout elements from XML and setup the toolbar
+     */
+    public void initializeScreen() {
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        /**
+         * Create SectionPagerAdapter, set it as adapter to viewPager with setOffscreenPageLimit(2)
+         **/
+        SectionPagerAdapter adapter = new SectionPagerAdapter(getSupportFragmentManager());
+        viewPager.setOffscreenPageLimit(2);
+        viewPager.setAdapter(adapter);
+        /**
+         * Setup the mTabLayout with view pager
+         */
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    /**
+     * SectionPagerAdapter class that extends FragmentStatePagerAdapter to save fragments state
+     */
+    public class SectionPagerAdapter extends FragmentStatePagerAdapter {
+
+        public SectionPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /**
+         * Use positions (0 and 1) to find and instantiate fragments with newInstance()
+         *
+         * @param position
+         */
+        @Override
+        public Fragment getItem(int position) {
+
+            Fragment fragment = null;
+
+            /**
+             * Set fragment to different fragments depending on position in ViewPager
+             */
+            switch (position) {
+                case 0:
+                    fragment = LoginFragment.newInstance();
+                    break;
+                case 1:
+                    fragment = SignUpFragment.newInstance();
+                    break;
+                default:
+                    fragment = LoginFragment.newInstance();
+                    break;
+            }
+
+            return fragment;
+        }
+
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        /**
+         * Set string resources as titles for each fragment by it's position
+         *
+         * @param position
+         */
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getString(R.string.pager_title_login);
+                case 1:
+                default:
+                    return getString(R.string.pager_title_signup);
+            }
+        }
     }
 }
