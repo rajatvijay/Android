9c70fc3e5fc803dac9f88462756b7b9b5b7849dd
Rajat Vijay <rajatvijay5@gmail.com>
2016-01-19 18:36:15 +0530

    Added code for drawer function and recycler view adapter with three fragments => Profile, Competition, Categories

--- app/src/main/java/www/rajat/gnosis/com/gnosis/ui/NavigationDrawerActivity.java
+++ app/src/main/java/www/rajat/gnosis/com/gnosis/ui/NavigationDrawerActivity.java
@@ -1,55 +1,85 @@
 package www.rajat.gnosis.com.gnosis.ui;
 
+import android.content.res.TypedArray;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import www.rajat.gnosis.com.gnosis.R;
 
 public class NavigationDrawerActivity extends AppCompatActivity {
 
-    private ListView categoryList;
-    private ListView navList;
-
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    RecyclerView recyclerView;
+    String navTitles[];
+    TypedArray navIcons;
+    RecyclerView.Adapter recyclerViewAdapter;
+    ActionBarDrawerToggle drawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_drawer);
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        //Let's first set up toolbar
+        setupToolbar();
+
+        //Initialize Views
+        recyclerView  = (RecyclerView) findViewById(R.id.recyclerView);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerMainActivity);
+
+        //Setup Titles and Icons of Navigation Drawer
+        navTitles = getResources().getStringArray(R.array.navDrawerItems);
+        navIcons = getResources().obtainTypedArray(R.array.navDrawerIcons);
+
+
+        /**
+         *Here , pass the titles and icons array to the adapter .
+         *Additionally , pass the context of 'this' activity .
+         *So that , later we can use the fragmentManager of this activity to add/replace fragments.
+         */
+
+        recyclerViewAdapter = new RecyclerViewAdapter(navTitles, navIcons, this);
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+        /**
+         *It is must to set a Layout Manager For Recycler View
+         *As per docs ,
+         *RecyclerView allows client code to provide custom layout arrangements for child views.
+         *These arrangements are controlled by the RecyclerView.LayoutManager.
+         *A LayoutManager must be provided for RecyclerView to function.
+         */
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //Finally setup ActionBarDrawerToggle
+        setupDrawerToggle();
+
+
+        //Add the Very First i.e Squad Fragment to the Container
+        Fragment profileFragment = new ProfileFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.containerView, profileFragment, null);
+        fragmentTransaction.commit();
+
+    }
+
+    void setupToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
-        String[] categories = {
-                "Sports",
-                "Music",
-                "Arts",
-                "Culture",
-                "College Tutors",
-                "Academics"
-        };
-
-        String[] navItems = {
-                "Profile",
-                "Contact Us",
-                "About Us"
-        };
-
-        categoryList = (ListView) findViewById(R.id.category_list);
-        ArrayAdapter<String> mCategoryAdapter = new ArrayAdapter<String>(this, R.layout.category_item, R.id.category_item_textview, categories);
-        categoryList.setAdapter(mCategoryAdapter);
-
-        navList = (ListView) findViewById(R.id.navList);
-        ArrayAdapter<String> mNavAdapter = new ArrayAdapter<String>(this, R.layout.nav_item, R.id.nav_item_textview, navItems);
-        navList.setAdapter(mNavAdapter);
-
+    void setupDrawerToggle(){
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.app_name,R.string.app_name);
+        //This is necessary to change the icon of the Drawer Toggle upon state change.
+        drawerToggle.syncState();
     }
 }
